a0bcbc37936f47a63af330d4506e35ac478a599d
NathanPinto22 <nathanpintogoa@gmail.com>
2025-08-14 13:25:03 +0530

    get and update team(s) api endpoints and crud, team pydantic schemas

--- server/app/main.py
+++ server/app/main.py
@@ -3,7 +3,7 @@
 # from server_utils import User, UserPydantic
 # import json
 from fastapi import FastAPI # type: ignore
-from app.api import users, leads, auth
+from app.api import users, leads, auth, teams
 from app.api import test_db
 from app.database.connection import database
 
@@ -24,7 +24,8 @@ async def shutdown():
     await database.disconnect()
 
 
-app.include_router(users.router)
-app.include_router(leads.router)
 app.include_router(auth.router)
 app.include_router(test_db.router, prefix="/debug", tags=["Debug"])
+app.include_router(users.router)
+app.include_router(leads.router)
+app.include_router(teams.router)
---
+++ server/app/services/team_service.py
@@ -0,0 +1,30 @@
+from fastapi import HTTPException
+from app.crud import team
+from app.schemas.team import TeamCreate, TeamUpdate
+from datetime import date
+from databases import Database
+
+
+async def get_team(db: Database, team_id: int):
+    return await team.get_team_by_id(db, team_id)
+
+from typing import Optional
+from datetime import date
+
+async def get_teams(db: Database, name: Optional[str] = None):
+    if name:
+        return await team.get_teams_by_name(db, name)
+    else:
+        return await team.get_teams(db)
+
+async def update_team(db: Database, team_id: str, teamObj: TeamUpdate):
+    result = await team.get_team_by_id(db, team_id)
+    if result is None:
+        raise HTTPException(status_code=404, detail="Team not found")
+
+    db_data = dict(result)
+
+    update_data = teamObj.model_dump(exclude_unset=True)
+    
+    team.update_team(db, update_data)
+    
