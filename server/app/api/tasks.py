8c7cd44e91733be9944f2b3277b9c2fc5c8673e7
NathanPinto22 <nathanpintogoa@gmail.com>
2025-08-14 17:46:38 +0530

    get task endpoints added, not fully functional date filter bug

---
+++ server/app/api/tasks.py
@@ -0,0 +1,40 @@
+from fastapi import APIRouter, Depends
+from databases import Database
+from app.database.connection import get_db
+from app.schemas.task import TaskCreate, TaskUpdate, TaskResponse
+from app.services import task_service
+from datetime import date, datetime
+
+router = APIRouter(prefix="/tasks", tags=["Tasks"])
+
+@router.post("/create-task", response_model=TaskResponse)
+async def create_task(task: TaskCreate, db: Database = Depends(get_db)):
+    return await task_service.create_task(db, task)
+
+@router.get("/", response_model=list[TaskResponse])
+async def get_task(
+    due_date: date = None,
+    status: str = None,
+    priority: str = None,
+    user_id: int = None,
+    opportunity_id: int = None,
+    before: bool = True, 
+    db: Database = Depends(get_db)
+):
+    return await task_service.get_tasks(db, due_date, status, priority, user_id, opportunity_id, before)
+
+
+@router.get("/task-{task_id}", response_model=TaskResponse)
+async def get_task(task_id: int, db: Database = Depends(get_db)):
+    return await task_service.get_task(db, task_id)
+
+
+@router.put("/{task_id}", response_model=TaskResponse)
+async def update_task(task_id: str, task: TaskUpdate):    
+    return await task_service.update_task(task_id, task)  # service function to be implemented
+
+
+
+# @router.delete("/{task_id}")
+# def delete_task(task_id: str):
+#     return task_service.delete_task(task_id)  # service function to be implemented
--- server/app/database/models.py
+++ server/app/database/models.py
@@ -1,6 +1,6 @@
 # app/database/models/user.py
 import sqlalchemy
-from sqlalchemy import Date, Float, ForeignKey, Table, Column, Integer, String, MetaData, Text
+from sqlalchemy import Date, Float, ForeignKey, Table, Column, Integer, String, MetaData, Text, DateTime
 
 metadata = MetaData()
 
@@ -23,6 +23,23 @@ Campaign = Table(
     Column("end_date", Date, nullable=True)
 )
 
+PipelineStage = Table(
+    "PipelineStage",
+    metadata,
+    Column("id", Integer, primary_key=True, index=True),
+    Column("name", String(50), nullable=False),
+    Column("order", Integer, nullable=False)
+)
+
+User = Table(
+    "User",
+    metadata,
+    Column("id", Integer, primary_key=True, index=True),
+    Column("name", String(100), nullable=False),
+    Column("role", String(20), nullable=False, default="Unassigned"),
+    Column("email", String(100), unique=True, nullable=True),
+    Column("team_id", Integer, ForeignKey("team.id"))
+)
 
 Lead = Table(
     "Lead",
@@ -36,16 +53,40 @@ Lead = Table(
     Column("score", Float, nullable=True, default=0.0),
     Column("created_at", Date, nullable=False),
     Column("updated_at", Date, nullable=False),
-    Column("team_id", Integer, ForeignKey("team.id")),
-    Column("user_id", Integer, ForeignKey("user.id"))
+    Column("team_id", Integer, ForeignKey(Team.c.id)),
+    Column("user_id", Integer, ForeignKey(User.c.id))
 )
 
-User = Table(
-    "User",
+
+LeadCampaign = Table(
+    "LeadCampaign",
+    metadata,
+    Column("lead_id", Integer, ForeignKey(Lead.c.id), nullable=False),
+    Column("campaign_id", Integer, ForeignKey(Campaign.c.id), nullable=False)
+)
+
+
+Opportunity = Table(
+    "Opportunity",
     metadata,
     Column("id", Integer, primary_key=True, index=True),
     Column("name", String(100), nullable=False),
-    Column("role", String(20), nullable=False, default="Unassigned"),
-    Column("email", String(100), unique=True, nullable=True),
-    Column("team_id", Integer, ForeignKey("team.id"))
+    Column("value", Float, nullable=True, default=0.0),
+    Column("close_date", Date, nullable=False),
+    Column("created_at", Date, nullable=False),
+    Column("lead_id", Integer, ForeignKey(Lead.c.id)),
+    Column("pipeline_stage_id", Integer, ForeignKey(PipelineStage.c.id))
+)
+
+Task = Table(
+    "Task",
+    metadata,
+    Column("id", Integer, primary_key=True, index=True),
+    Column("title", String(100), nullable=False),
+    Column("description", String, nullable=False),
+    Column("due_date", DateTime, nullable=False),
+    Column("priority", String(50), nullable=False, default="Low"),
+    Column("status", String(50), nullable=False, default="Pending"),
+    Column("user_id", Integer, ForeignKey(Lead.c.id)),
+    Column("opportunity_id", Integer, ForeignKey(PipelineStage.c.id))
 )
