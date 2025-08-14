08f47df32cf834a12668e8b748aae3e9b3d7b57e
SarveshSavaikar <sarveshsavaikar93@gmail.com>
2025-08-14 18:15:17 +0530

    Update jwt_middleware.py

--- server/app/middlewares/jwt_middleware.py
+++ server/app/middlewares/jwt_middleware.py
@@ -10,7 +10,7 @@ ALGORITHM = "HS256"
 
 class JWTMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        # Skip authentication for /auth routes
+        # here i am skipping this path for the middleware currently 
         if (
             request.url.path.startswith("/auth") or
     request.url.path.startswith("/docs") or
@@ -26,7 +26,7 @@ class JWTMiddleware(BaseHTTPMiddleware):
         token = auth_header.split(" ")[1]
         try:
             payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-            request.state.user = payload  # store payload for later use in routes
+            request.state.user = payload  # payload is stored here and can be used outside where ever reuired 
         except jwt.ExpiredSignatureError:
             return JSONResponse(status_code=401, content={"detail": "Token expired"})
         except jwt.InvalidTokenError:
