23c63a7663a8297f679e7de033769fb9d4b5215a
NathanPinto22 <nathanpintogoa@gmail.com>
2025-08-22 17:59:18 +0530

    get opportunities(list or count) by month single/all

--- server/app/api/opportunities.py
+++ server/app/api/opportunities.py
@@ -42,3 +42,10 @@ async def update_opportunity(opportunity_id: int, opportunity: OpportunityUpdate
 async def delete_opportunity(opportunity_id: int, db: Database = Depends(get_db)):
     return await opportunity_service.delete_opportunity(db, opportunity_id)
 
+@router.get("/by-month")
+async def get_deals_by_month_all(count: bool = False, db: Database = Depends(get_db)):
+    return await opportunity_service.get_deals_by_month(db, count)
+
+@router.get("/by-month/{month}")
+async def get_deals_by_month(month: int = datetime.now().month, count: bool = False, db: Database = Depends(get_db)):
+    return await opportunity_service.get_deals_by_month(db, month, count)
